0f33ce0252042d5e17e762fb1ddd8ad6f1e70887
Soljes <andreysoljesid@gmail.com>
2026-09-07 14:33:00 +0300

    Обновил файлы в Вебинаре 3

--- Вебинар 3. Списки и кортежи/task1/task.py
+++
@@ -1,8 +0,0 @@
-def modification(lst):
-    """Изменение списка
-    :param lst: список
-    :return: преобразованный список
-    """
-    # todo Здесь нужно написать код
-    lst[0], lst[-1] = lst[-1], lst[0]
-    return lst
