7bdf288468f61dab2cdf64d23b5ce726091ef44e
Soljes <andreysoljesid@gmail.com>
2026-09-07 14:43:02 +0300

    Выполнил домашнее задание к Вебинару 3

--- Вебинар 3. Списки и кортежи/task2.py
+++ Вебинар 3. Списки и кортежи/task2.py
@@ -3,7 +3,10 @@
 # сумму и среднее арифметическое с округлением до 2 знаков после запятой;
 
 def get_list_info(lst):
-    # Здесь пишем код
+    min_elem = min(lst)
+    max_elem = max(lst)
+    sum_list = lst[0] + lst[1] + lst[2] + lst[3] + lst[4] + lst[5] + lst[6]
+    average = round(sum_list / len(lst), 2)
     return min_elem, max_elem, sum_list, average
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
